7ccc6bb99360108d21610d3d01d6351d5e6318e7
Duy Nguyen <duyng93@gmail.com>
2018-04-05 12:05:00 -0700

    Setting up validation functions for signup form

--- server/public/signup.js
+++ server/public/signup.js
@@ -1,9 +1,20 @@
 $(document).ready(()=> {
     setTimeout(transitionScreen, 1000);
+    initializeApp();
 });
 
 function transitionScreen(){
     $('.loading').fadeOut(3000);
 
+}
+
+function initializeApp() {
+    getEmailInput();
+    getUsernameInput();
+    getPasswordInput();
+}
+
+function getEmailInput() {
+    
 }
 
