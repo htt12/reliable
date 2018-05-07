59087ef851489ce3d544806edc0fe919865f8cb0
Duy Nguyen <duyng93@gmail.com>
2018-05-07 13:16:53 -0700

    Responsive design get all pages to look good on screen with min-width of 320px and 480px

--- server/public/js/dashboard.js
+++ server/public/js/dashboard.js
@@ -74,7 +74,7 @@ function getData(){
         success: function(resp){
             console.log('dashboard',resp);
 
-            // rendergoalOnDashboard(resp.data)
+            rendergoalOnDashboard(resp.data)
         },
         error: function(xhr, status, err){
             console.log(err)
@@ -83,88 +83,88 @@ function getData(){
 }
 
 // ***************** TESTING *************************
-var object = [
-    {
-        goal: 'make it work make it work make it work make it work make it work make it work make it work make it work make it work make it work',
-        goal_id: 88,
-        day: 3,
-        timeframe: 2
-    },
-    {
-        goal: 'make $$$$$$$$$$$$$$$$$$$$$$$',
-        goal_id: 8,
-        day: 3,
-        timeframe: 3
-    },
-    {
-        goal: 'make it work',
-        goal_id: 18,
-        day: 5,
-        timeframe: 2
-    },
-    {
-        goal: 'make $$$$$$$$$$$$$$$$$$$$$$$',
-        goal_id: 8,
-        day: 2,
-        timeframe: 1
-    },
-    {
-        goal: 'make it work make it work make it work',
-        goal_id: 10,
-        day: 4,
-        timeframe: 2
-    },
-    {
-        goal: 'make $$$$$$$$$$$$$$$$$$$$$$$',
-        goal_id: 8,
-        day: 5,
-        timeframe: 2
-    },
-    {
-        goal: 'make it work make it work make it work',
-        goal_id: 88,
-        day: 2,
-        timeframe: 1
-    },
-    {
-        goal: 'make $$$$$$$$$$$$$$$$$$$$$$$',
-        goal_id: 8,
-        day: 5,
-        timeframe: 2
-    },
-    {
-        goal: 'make it work make it work make it work',
-        goal_id: 88,
-        day: 2,
-        timeframe: 1
-    },
-    {
-        goal: 'make $$$$$$$$$$$$$$$$$$$$$$$',
-        goal_id: 8,
-        day: 5,
-        timeframe: 2
-    },
-    {
-        goal: 'make it work make it work make it work',
-        goal_id: 88,
-        day: 2,
-        timeframe: 1
-    },
-    {
-        goal: 'make $$$$$$$$$$$$$$$$$$$$$$$',
-        goal_id: 8,
-        day: 5,
-        timeframe: 2
-    },
-    {
-        goal: 'make it work make it work make it work',
-        goal_id: 88,
-        day: 2,
-        timeframe: 1
-    }
-]
+// var object = [
+//     {
+//         goal: 'make it work make it work make it work make it work make it work make it work make it work make it work make it work make it work',
+//         goal_id: 88,
+//         day: 3,
+//         timeframe: 2
+//     },
+//     {
+//         goal: 'make $$$$$$$$$$$$$$$$$$$$$$$',
+//         goal_id: 8,
+//         day: 3,
+//         timeframe: 3
+//     },
+//     {
+//         goal: 'make it work',
+//         goal_id: 18,
+//         day: 5,
+//         timeframe: 2
+//     },
+//     {
+//         goal: 'make $$$$$$$$$$$$$$$$$$$$$$$',
+//         goal_id: 8,
+//         day: 2,
+//         timeframe: 1
+//     },
+//     {
+//         goal: 'make it work make it work make it work',
+//         goal_id: 10,
+//         day: 4,
+//         timeframe: 2
+//     },
+//     {
+//         goal: 'make $$$$$$$$$$$$$$$$$$$$$$$',
+//         goal_id: 8,
+//         day: 5,
+//         timeframe: 2
+//     },
+//     {
+//         goal: 'make it work make it work make it work',
+//         goal_id: 88,
+//         day: 2,
+//         timeframe: 1
+//     },
+//     {
+//         goal: 'make $$$$$$$$$$$$$$$$$$$$$$$',
+//         goal_id: 8,
+//         day: 5,
+//         timeframe: 2
+//     },
+//     {
+//         goal: 'make it work make it work make it work',
+//         goal_id: 88,
+//         day: 2,
+//         timeframe: 1
+//     },
+//     {
+//         goal: 'make $$$$$$$$$$$$$$$$$$$$$$$',
+//         goal_id: 8,
+//         day: 5,
+//         timeframe: 2
+//     },
+//     {
+//         goal: 'make it work make it work make it work',
+//         goal_id: 88,
+//         day: 2,
+//         timeframe: 1
+//     },
+//     {
+//         goal: 'make $$$$$$$$$$$$$$$$$$$$$$$',
+//         goal_id: 8,
+//         day: 5,
+//         timeframe: 2
+//     },
+//     {
+//         goal: 'make it work make it work make it work',
+//         goal_id: 88,
+//         day: 2,
+//         timeframe: 1
+//     }
+// ]
 
-rendergoalOnDashboard(object);
+// rendergoalOnDashboard(object);
 
 // ***************************************************
 
